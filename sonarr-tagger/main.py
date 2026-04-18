f7782e20c27ff5ee5120d063fbb35ddf09f0407e
silentzane <vacantzane@gmail.com>
2026-04-18 16:42:27 -0500

    Updates version.

--- sonarr-tagger/main.py
+++ sonarr-tagger/main.py
@@ -162,7 +162,7 @@ def get_score_tag(score: int, threshold: int) -> str:
         return "positive-score"
     return "no-score"
 
-VERSION = "1.0.6"
+VERSION = "1.0.7"
 
 @dataclass
 class SonarrContext:
